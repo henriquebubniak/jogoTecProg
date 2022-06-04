d8c515623d3176192f1d0da25d77190a32b68e73
Henrique  Zambenedetti <hique.rz03@gmail.com>
2022-06-03 22:41:07 -0300

    alteração na construtora da classe Militar1: adição da linha 'camarada = g'

--- Militar1.cpp
+++ Militar1.cpp
@@ -4,9 +4,10 @@
 
 /*=======================CONSTRUTORA E DESTRUTORA==============================*/
 Militar1::Militar1(Camarada* g):
-    Inimigo(Vector2f(WINDOW_SIZEX - 100.f, WINDOW_SIZEY - 400.f), Vector2f(50.f, 50.f), 100, 0.3, g)
+    Inimigo(Vector2f(WINDOW_SIZEX - 300.f, WINDOW_SIZEY - 400.f), Vector2f(50.f, 50.f), 100, 0.3, g)
 {
     caixa.setFillColor(Color::Blue);
+    camarada = g;
 }
 
 Militar1::~Militar1()
